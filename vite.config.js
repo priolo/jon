965c6caeae5e372b0df0c4fb94d08db741f22bf3
priolo <iorioivano@gmail.com>
2022-09-11 16:03:13 +0200

    macche' pero' non importa

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,30 @@
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.js'),
       name: 'index',
-      // the proper extensions will be added
       fileName: 'index'
     },
-	
-	outDir: resolve(__dirname, './dist'),
-    // rollupOptions: {
-    //   output: {
-	// 	dir: './dist',
-    //   }
-    // }
-  }
+    outDir: resolve(__dirname, './dist'),
+  },
   // resolve: {
-    //     alias: [
-    //         // put your alias here
-    //     ]
-    // },
+  //     alias: [
+  //         // put your alias here
+  //     ]
+  // },
+  plugins: [react()],
+  // rollupOptions: {
+  //   external: ['react', 'react-dom'],
+  //   output: {
+  //       globals: {
+  //           react: 'React',
+  //           'react-dom': 'ReactDOM',
+  //           'styled-components': 'styled',
+  //       },
+  //   },
+  // }
 })
